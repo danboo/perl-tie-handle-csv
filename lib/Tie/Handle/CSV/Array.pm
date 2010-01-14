a91a9bef81fb80022b7be994991d22dc5e6fdc75
Dan Boorstein <danboo@danboo-desktop.(none)>
2010-01-13 21:54:23 -0500

    - initial import

---
+++ lib/Tie/Handle/CSV/Array.pm
@@ -0,0 +1,70 @@
+package Tie::Handle::CSV::Array;
+
+use 5.006;
+use strict;
+use warnings;
+
+use overload '""' => \&_stringify, fallback => 1;
+
+sub _new
+   {
+   my ($class, $parent) = @_;
+   my @self;
+   tie(@self, $class, $parent);
+   bless \@self, $class;
+   }
+
+sub TIEARRAY
+   {
+   my ($class, $parent) = @_;
+   return bless { data => [], parent => $parent }, $class;
+   }
+
+sub CLEAR
+   {
+   my ($self) = @_;
+   @{ $self->{'data'} } = ();
+   }
+
+sub EXTEND
+   {
+   my ($self, $count) = @_;
+   }
+
+sub STORE
+   {
+   my ($self, $index, $value) = @_;
+   $self->{'data'}[$index] = $value;
+   }
+
+sub FETCHSIZE
+   {
+   my ($self) = @_;
+   return scalar @{ $self->{'data'} };
+   }
+
+sub FETCH
+   {
+   my ($self, $index) = @_;
+   return $self->{'data'}[$index];
+   }
+
+sub _stringify
+   {
+   my ($self) = @_;
+   my $under_tie = tied @{ $self };
+   my @values = @{ $under_tie->{'data'} };
+   $under_tie->{'parent'}{'opts'}{'csv_parser'}->combine(@values)
+      || croak $under_tie->{'parent'}{'opts'}{'csv_parser'}->error_input();
+   return $under_tie->{'parent'}{'opts'}{'csv_parser'}->string();
+   }
+
+1;
+
+__END__
+
+=head1 NAME
+
+Tie::Handle::CSV::Array - Support class for L<Tie::Handle::CSV>
+
+=cut
---
+++ lib/Tie/Handle/CSV/Hash.pm
@@ -0,0 +1,99 @@
+package Tie::Handle::CSV::Hash;
+
+use 5.006;
+use strict;
+use warnings;
+
+use Carp 'cluck';
+
+use overload '""' => \&_stringify, fallback => 1;
+
+sub _new
+   {
+   my ($class, $parent) = @_;
+   my %self;
+   tie(%self, $class, $parent);
+   bless \%self, $class;
+   }
+
+sub TIEHASH
+   {
+   my ($class, $parent) = @_;
+   my $self = bless { data => {}, parent => $parent }, $class;
+   $self->{'lc'} = lc $parent->{'opts'}{'key_case'} eq 'any';
+   return $self;
+   }
+
+sub STORE
+   {
+   my ($self, $key, $value) = @_;
+   $key = $self->{'lc'} ? lc $key : $key;
+   $self->{'data'}{$key} = $value;
+   }
+
+sub FETCH
+   {
+   my ($self, $key) = @_;
+   $key = $self->{'lc'} ? lc $key : $key;
+   return $self->{'data'}{$key};
+   }
+
+sub EXISTS
+   {
+   my ($self, $key) = @_;
+   $key = $self->{'lc'} ? lc $key : $key;
+   exists $self->{'data'}{$key};
+   }
+
+sub DELETE
+   {
+   my ($self, $key) = @_;
+   $key = $self->{'lc'} ? lc $key : $key;
+   delete $self->{'data'}{$key};
+   }
+
+sub CLEAR
+   {
+   my ($self) = @_;
+   %{ $self->{'data'} } = ();
+   }
+
+sub FIRSTKEY
+   {
+   my ($self) = @_;
+   $self->{'keys'} = [ @{ $self->{'parent'}{'opts'}{'header'} } ];
+   return shift @{ $self->{'keys'} };
+   }
+
+sub NEXTKEY
+   {
+   my ($self) = @_;
+   @{ $self->{'keys'} }
+      ? return shift @{ $self->{'keys'} }
+      : return;
+   }
+
+sub _stringify
+   {
+   my ($self) = @_;
+   my $under_tie = tied %$self;
+   my @keys   = @{ $under_tie->{'parent'}{'opts'}{'header'} };
+   if ($under_tie->{'lc'})
+      {
+      @keys = map lc, @keys;
+      }
+   my @values = @{ $under_tie->{'data'} }{ @keys };
+   $under_tie->{'parent'}{'opts'}{'csv_parser'}->combine(@values)
+      || croak $under_tie->{'parent'}{'opts'}{'csv_parser'}->error_input();
+   return $under_tie->{'parent'}{'opts'}{'csv_parser'}->string();
+   }
+
+1;
+
+__END__
+
+=head1 NAME
+
+Tie::Handle::CSV::Hash - Support class for L<Tie::Handle::CSV>
+
+=cut
